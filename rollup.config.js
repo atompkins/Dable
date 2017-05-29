57859477b46d36dc623956d0df6e761a9ee4d33f
atompkins <atompkins@gmail.com>
2017-05-29 22:00:13 +0100

    istanbul coverage reporting

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,27 @@
-import {readFileSync} from 'fs';
+import istanbul from 'rollup-plugin-istanbul';
+// import {readFileSync} from 'fs';
 
-var pkg = JSON.parse(readFileSync('bower.json', 'utf-8'));
-var banner = '/*! ' + pkg.name + ' v' + pkg.version +
-  ' (' + pkg.homepage + ') */';
+// var pkg = JSON.parse(readFileSync('bower.json', 'utf-8'));
+const bwr = require('./bower.json');
+const banner = '/*! ' + bwr.name + ' v' + bwr.version +
+  ' (' + bwr.homepage + ') */';
+
+let plugins = [];
+let dest = 'lib/dable.js';
+
+if (process.env.BUILD === 'test') {
+  plugins.push(istanbul({include: ['src/**/*.js']}));
+  dest = 'coverage/dable.test.js';
+}
 
 export default {
-  entry: 'src/dable.js',
   banner: banner,
+  entry: 'src/dable.js',
+  legacy: true,
   moduleId: 'Dable',
   moduleName: 'Dable',
-  legacy: true,
+  plugins: plugins,
   targets: [
-    {dest: 'lib/dable.js', format: 'umd'}
+    {dest: dest, format: 'umd'}
   ]
 };
